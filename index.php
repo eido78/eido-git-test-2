56e67b5df52efd874480970f485ceb7965f51bb4
eido78 <okeada78@gmail.com>
2023-06-09 03:27:57 +0700

    menambahkan list baru dengan ul dan li satu dua tiga

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut vero optio praesentium repellendus laudantium nulla, nemo aspernatur beatae alias nostrum architecto aliquid voluptas culpa, fuga nam totam tempore error eum.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 
 </html>
